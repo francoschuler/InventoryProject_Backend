143409c0d68eaba56a70edf1bf49189d9b6fe608
FRANCO EXEQUIEL SCHULER ALLUB <franschuler13@gmail.com>
2022-06-29 16:00:05 +0200

    Base structure project commit

---
+++ src/test/java/com/company/InventoryBackend/InventoryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.InventoryBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
